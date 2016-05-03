2860afca41ac269270fb1d0b963bc61677c4a6a7
jessecusack <jesse.cusack@noc.soton.ac.uk>
2016-05-03 14:44:05 +0100

    included docstring and accepts multiple save file types

--- my_savefig.py
+++ my_savefig.py
@@ -9,9 +9,34 @@ import matplotlib.pyplot as plt
 import os
 
 
-def savefig(fig, fid, fname, sdir, fsize=None, lock_aspect=True, ftype='png',
-            font_size=None):
-    """Savefig function that rescales plots for publication in AMS journals."""
+def my_savefig(fig, fid, fname, sdir, fsize=None, lock_aspect=True,
+               ftype='png', font_size=None, **kwargs):
+    """Savefig function that rescales plots for publication in AMS journals.
+
+    Parameters
+    ----------
+    fig: figure object
+        Figure to save.
+    fid:
+        Figure identifyer 1.
+    name:
+        Figure name. (To be merged with fid in future.)
+    sdir: string
+        Save directory.
+    fsize: tuple
+        Figure size in inches, e.g. (4, 2). Alternatively, pass 'single_col' or
+        'double_col' to use AMS double and single column widths which are 3.125
+        and 6 inches.
+    lock_aspect: boolean, optional
+        Fix aspect ratio on resize, true by default.
+    ftype: string, optional
+        Save file type, 'png' by default.
+    font_size: int
+        Universal font size for all figure elements. Currently not working.
+    kwargs: optional
+        Additional arguments to the savefig function.
+
+    """
 
     scol = 3.125  # inches
     dcol = 6.5  # inches
@@ -46,5 +71,13 @@ def savefig(fig, fid, fname, sdir, fsize=None, lock_aspect=True, ftype='png',
         fig.canvas.draw()
 
     fname = str(fid) + '_' + fname
-    fname = os.path.join(sdir, fname) + '.' + ftype
-    plt.savefig(fname, dpi=300., bbox_inches='tight')
+
+    if type(ftype) is tuple:
+        for ft in ftype:
+            fn = os.path.join(sdir, fname) + '.' + ft
+            plt.savefig(fn, dpi=300., bbox_inches='tight', pad_inches=0.,
+                        **kwargs)
+    else:
+        fname = os.path.join(sdir, fname) + '.' + ftype
+        plt.savefig(fname, dpi=300., bbox_inches='tight', pad_inches=0.,
+                    **kwargs)
