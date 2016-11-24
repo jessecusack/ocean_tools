87d7df7b778c5ef4b3a560e1f69f693b58bb0672
jessecusack <jesse.cusack@noc.soton.ac.uk>
2016-11-24 16:12:32 +0000

    alternative loadmat function added

--- utils.py
+++ utils.py
@@ -10,6 +10,7 @@ all into one miscellaneous 'utilities' module.
 """
 
 import numpy as np
+import scipy.io as spio
 from datetime import datetime, timedelta
 
 
@@ -454,3 +455,42 @@ def spherical_polar_area(r, lon, lat):
     solid_angle = gdlon.T*gdlat.T*np.cos(np.deg2rad(lat))
 
     return solid_angle.T*r**2
+
+
+def loadmat(filename):
+    '''
+    Big thanks to mergen on stackexchange for this:
+        http://stackoverflow.com/a/8832212
+
+    This function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects.
+    '''
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict):
+    '''
+    Checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries.
+    '''
+    for key in dict:
+        if isinstance(dict[key], spio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+
+def _todict(matobj):
+    '''
+    A recursive function which constructs from matobjects nested dictionaries.
+    '''
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
